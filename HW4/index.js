099449043057e55884d078acbdb7a91521552b6b
IKFor <85750192+IKFor@users.noreply.github.com>
2023-07-03 16:52:29 +0100

    HW 4(1)

--- HW4/index.js
+++ HW4/index.js
@@ -1,1 +1,20 @@
 //#1
+
+let headerTwo = document.getElementById("headerTwo");
+console.log(headerTwo);
+
+let section = document.querySelector("section");
+console.log(section);
+
+let li;
+
+for (const element of document.querySelectorAll("ul > li")) {
+    if (element.innerHTML === "Пункт 5") {
+        li = element;
+        break;
+    }
+}
+console.log(li);
+
+let spanWithClass = document.getElementsByClassName("hatredLevelCounter");
+console.log(section);
